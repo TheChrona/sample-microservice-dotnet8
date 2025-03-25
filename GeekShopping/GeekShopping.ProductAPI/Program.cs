c24451a85777292047a41fff1bb2c87ad1aa86de
Daniela Silva <daniela.domingos.silva@outlook.com.br>
2025-03-25 18:05:49 -0300

    migrations done for the first time. swagger configured

--- GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,11 +1,34 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
+    connection, 
+    ServerVersion.AutoDetect(connection)
+));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { 
+        Title = "GeekShopping", 
+        Version = "v1",
+        Description = "This is an API for GeekShopping, a study project",
+        Contact = new OpenApiContact
+        {
+            Name = "Daniela Silva",
+            Email = "daniela.domingos.silva@outlook.com.br"
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -13,7 +36,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "GeekShopping.ProductAPI v1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseAuthorization();
