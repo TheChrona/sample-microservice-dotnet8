ee6a67e60cead7dd7162f0ad378b43a952026e0b
Daniela Silva <daniela.domingos.silva@outlook.com.br>
2025-03-28 18:13:44 -0300

    Adding API as a service to Frontend

---
+++ GeekShopping/GeekShopping.Web/Services/Interfaces/ProductService.cs
@@ -0,0 +1,57 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Utils;
+
+namespace GeekShopping.Web.Services.Interfaces
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _httpClient;
+        public const string BasePath = "api/v1/product";
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<ProductViewModel>> FindAllProducts()
+        {
+            var response = await _httpClient.GetAsync(BasePath);
+            return await response.ReadContentAs<IEnumerable<ProductViewModel>>();
+       }
+
+        public async Task<ProductViewModel> FindProductById(long id)
+        {
+            var response = await _httpClient.GetAsync($"{BasePath}/{id}");
+            return await response.ReadContentAs<ProductViewModel>();
+        }
+        public async Task<ProductViewModel> CreateProduct(ProductViewModel model)
+        {
+            var response = await _httpClient.PostAsJsonAsync(BasePath, model);
+            if(response.IsSuccessStatusCode)
+            {
+                return await response.ReadContentAs<ProductViewModel>();
+            } else
+            {
+                throw new Exception("Something went wrong when calling API");
+            }
+        }
+        public async Task<ProductViewModel> UpdateProduct(ProductViewModel model)
+        {
+            var response = await _httpClient.PutAsJsonAsync(BasePath, model);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.ReadContentAs<ProductViewModel>();
+            }
+            else
+            {
+                throw new Exception("Something went wrong when calling API");
+            }
+        }
+
+        public async Task<bool> DeleteProductById(long id)
+        {
+            var response = await _httpClient.DeleteAsync($"{BasePath}/{id}");
+            return await response.ReadContentAs<bool>();
+        }
+    }
+}
---
+++ GeekShopping/GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace GeekShopping.Web.Utils
+{
+    public static class HttpClientExtensions
+    {
+        private static MediaTypeHeaderValue contentType = new("application/json");
+        private static JsonSerializerOptions jsonSerializeOptions = new() { PropertyNameCaseInsensitive = true };
+
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException($"Something went wrong calling the API: {response.ReasonPhrase}");
+            }
+            string dataAsString = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(dataAsString, jsonSerializeOptions);
+        }
+
+        public static async Task<HttpResponseMessage> PostAsJsonAsync<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return await httpClient.PostAsJsonAsync(url, content);
+        }
+
+        public static async Task<HttpResponseMessage> PutAsJsonAsync<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = contentType;
+            return await httpClient.PutAsJsonAsync(url, content);
+        }
+    }
+}
